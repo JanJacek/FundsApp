e142d1c69f2808568353b4647931b5bf2422ec99
JanJacek <bobkiewicz.jacek@gmail.com>
2026-03-04 18:04:29 +0100

    Add prod script

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const base = process.env.GITHUB_ACTIONS && repoName ? `/${repoName}/` : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [
     vue(),
     vueDevTools(),
