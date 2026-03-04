90d0a2508eaf57952cac7b61755b7d2369ce3daf
JanJacek <bobkiewicz.jacek@gmail.com>
2026-03-04 16:57:16 +0100

    Add tailwind & scss

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { useAuthStore } from '@/stores/auth'
+import './styles/main.scss'
 
 const app = createApp(App)
 const pinia = createPinia()
